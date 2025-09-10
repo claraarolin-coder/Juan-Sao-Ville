1a9831779e2401792ca4bf365da9d1c825c26ab8
Clara Rolin <claraa.rolin@gmail.com>
2025-09-11 01:34:08 +0200

    Fix CORS pour frontend Vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "https://juan-sao-ville.vercel.app/login"],  # ports possibles de Vite
+    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "https://juan-sao-ville.vercel.app"],  # ports possibles de Vite
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
